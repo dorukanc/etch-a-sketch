3cb8bf68c363acabe67d00bb0ee9e025935e9045
Dorukan <catakd@mef.edu.tr>
2025-09-26 12:52:09 +0300

    add adjust grid button, update style

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 const container = document.getElementById('container');
+const adjust = document.getElementById('adjust');
 
-for(let i=1; i<=16; i++){
-    const row = document.createElement('div');
-    row.className = 'row';
-    container.appendChild(row);
+// init grid 16 * 16 default
+generateGrid(16, 16);
 
-    for(let j=1; j<=16; j++){
-        const square = document.createElement('div');
-        square.className = 'square';  
-        row.appendChild(square);  
-        console.log('append square');    
+
+function getUserInput(){
+    let colSize = parseInt(prompt('Please enter the col size:', '16'), 10) || 16; // add default vals
+    let rowSize = parseInt(prompt('Please enter the row size:', '16'), 10) || 16; 
+
+    colSize = Math.max(1, Math.min(100, isNaN(colSize) ? 16 : colSize));
+    rowSize = Math.max(1, Math.min(100, isNaN(colSize) ? 16 : rowSize));
+
+    return {colSize, rowSize};
+}
+
+
+function generateGrid(colSize, rowSize){
+    for(let i=1; i<=colSize; i++){
+        const col = document.createElement('div');
+        col.className = 'col';
+        container.appendChild(col);
+
+        for(let j=1; j<=rowSize; j++){
+            const square = document.createElement('div');
+            square.className = 'square';  
+            col.appendChild(square);  
+
+            square.addEventListener('mouseover', function(){
+            this.style.backgroundColor = 'lightblue';
+            });
+
+            console.log('append square');    
+        }
     }
 }
+
+adjust.addEventListener('click', function(){
+    let userInput = getUserInput();
+    generateGrid(userInput.colSize, userInput.rowSize);
+});
